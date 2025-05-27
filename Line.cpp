4c08440b1a042e8aa03c934f3f4023654ff7b391
KoudaAyu <k024g0061@g.neec.ac.jp>
2025-05-28 00:47:00 +0900

    2-3完了

---
+++ Line.cpp
@@ -0,0 +1,49 @@
+﻿#include "Line.h"
+#include"Plane.h"
+#include"Vector.h"
+
+bool IsCollisionSegment(const Segment& segment, const Plane& plane)
+{
+    //まず垂直判定を行う為に、法線と線の内積を求める
+    float dot = VectorDot(plane.normal, segment.diff);
+
+    //垂直 = 平行であるので衝突しているはずがない
+    if (fabs(dot) < 1e-6f)
+    {
+        return false;
+    }
+
+    float t = (plane.distance - VectorDot(segment.origin, plane.normal)) / dot;
+
+    if (t >= 0.0f && t <= 1.0f)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+
+void SegmentDraw(const Segment& segment, const Matrix4x4& viewProjectionMatrix, const Matrix4x4& viewportMatrix, uint32_t color)
+{
+    Vector3 start = segment.origin;
+    Vector3 end = {
+        segment.origin.x + segment.diff.x,
+        segment.origin.y + segment.diff.y,
+        segment.origin.z + segment.diff.z
+    };
+
+    // 変換（viewProjection → viewport）
+    Vector3 clipStart = VectorTransform(start, viewProjectionMatrix);
+    Vector3 screenStart = VectorTransform(clipStart, viewportMatrix);
+
+    Vector3 clipEnd = VectorTransform(end, viewProjectionMatrix);
+    Vector3 screenEnd = VectorTransform(clipEnd, viewportMatrix);
+
+    // 描画
+    Novice::DrawLine(
+        static_cast<int>(screenStart.x), static_cast<int>(screenStart.y),
+        static_cast<int>(screenEnd.x), static_cast<int>(screenEnd.y),
+        color
+    );
+}
